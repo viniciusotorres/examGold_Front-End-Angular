cd7d3b5b528363635ee9c075539d09b26459ef48
viniciusotorres <viniciustorres29@gmail.com>
2024-08-26 18:54:43 -0300

    Criando Crud, código inicial, em progresso...

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,7 @@ import {LoginComponent} from "./components/login/login.component";
 import {AuthGuard} from "./guards/auth.guard";
 import {HomeComponent} from "./components/home/home.component";
 import {RegisterComponent} from "./components/register/register.component";
+import {EscolasComponent} from "./components/escolas/escolas.component";
 
 export const routes: Routes = [
   {
@@ -18,7 +19,11 @@ export const routes: Routes = [
     path: 'home',
     component: HomeComponent,
     canActivate: [AuthGuard]
-
+  },
+  {
+    path: 'escolas',
+    component: EscolasComponent,
+    canActivate: [AuthGuard]
   }
 ];
 
